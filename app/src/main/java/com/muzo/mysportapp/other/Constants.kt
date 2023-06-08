774c6a58fb5262bcabaab60368d62b17df336830
erkocali1 <muhammedalierkoc92@gmail.com>
2023-06-08 17:34:51 +0300

    Tracking service created

--- app/src/main/java/com/muzo/mysportapp/other/Constants.kt
+++ app/src/main/java/com/muzo/mysportapp/other/Constants.kt
@@ -5,4 +5,13 @@ object Constants {
     const val RUNNING_DATABASE_NAME ="running_db"
 
     const val REQUEST_CODE_LOCATION_PERMISSION=0
+
+    const val ACTION_START_OR_RESUME_SERVICE="ACTION_START_OR_RESUME_SERVİCE"
+    const val ACTION_PAUSE_SERVICE="ACTION_START_OR_RESUME_SERVİCE"
+    const val ACTION_STOP_SERVICE="ACTION_START_OR_RESUME_SERVİCE"
+
+    const val NOTIFICATION_CHANNEL_ID="tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME="Tracking"
+    const val NOTIFICATION_ID=1
+
 }
--- app/src/main/java/com/muzo/mysportapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/muzo/mysportapp/ui/fragments/TrackingFragment.kt
@@ -1,30 +1,32 @@
 package com.muzo.mysportapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
-import com.muzo.mysportapp.R
 import com.muzo.mysportapp.databinding.FragmentTrackingBinding
+import com.muzo.mysportapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.muzo.mysportapp.services.TrackingService
 import com.muzo.mysportapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment() {
-    private lateinit  var binding:FragmentTrackingBinding
+    private lateinit var binding: FragmentTrackingBinding
     private val viewModel: MainViewModel by viewModels()
-    private var map:GoogleMap?=null
+    private var map: GoogleMap? = null
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        binding=FragmentTrackingBinding.inflate(layoutInflater,container,false)
+        binding = FragmentTrackingBinding.inflate(layoutInflater, container, false)
         return binding.root
 
     }
@@ -32,12 +34,20 @@ class TrackingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.mapView.onCreate(savedInstanceState)
-
+        binding.btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
         binding.mapView.getMapAsync {
-            map=it
+            map = it
         }
 
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireActivity(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
 
     override fun onResume() {
         super.onResume()
@@ -51,17 +61,17 @@ class TrackingFragment : Fragment() {
 
     override fun onStop() {
         super.onStop()
-        binding.mapView?.onStop()
+        binding.mapView.onStop()
     }
 
     override fun onPause() {
         super.onPause()
-        binding.mapView?.onPause()
+        binding.mapView.onPause()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        binding.mapView?.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 
 //    override fun onDestroy() {
